b1de98d2ff544f00402dec829686714bbf8d1f75
Zhulik <ulikk@bk.ru>
2024-09-09 09:23:50 +0300

    feat(): added scanner close

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number: ");
+        scanner.close();
     }
 }
